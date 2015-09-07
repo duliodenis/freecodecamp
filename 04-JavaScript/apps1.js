f1f64f4abc0c5349c9559a0a40b614cc02f558f2
Dulio Denis <dulio.denis@yahoo.com>
2015-09-07 14:24:55 -0400

    122. Check the Length Property of a String Variable

--- 04-JavaScript/apps1.js
+++ 04-JavaScript/apps1.js
@@ -1,11 +1,16 @@
-// name = "Alan Turing";
-// var firstName = "Alan";
-// var lastName = "Turing";
-var myFirstName = "Dulio";
-var myLastName = "Denis";
+var firstNameLength = 0;
+var lastNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+var lastName = "Lovelace";
+
+// don't change code above here
+
+lastNameLength = lastName.length;
 
 // Only change code above this line.
 // We use this function to show you the value of your variable in your output box.
 // You'll learn about functions soon.
-if(typeof(myFirstName) !== "undefined" && typeof(myLastName) !== "undefined"){(function(){return myFirstName + ', ' + myLastName;})();}
-
+if(typeof(lastNameLength) !== "undefined"){(function(){return lastNameLength;})();}
