f29f5a2a5b5b9f6070662bd747f8d21eea7b8bb6
Dulio Denis <dulio.denis@yahoo.com>
2015-09-07 13:54:32 -0400

    121. Declare String Variables

--- 04-JavaScript/apps1.js
+++ 04-JavaScript/apps1.js
@@ -1,19 +1,11 @@
-function welcomeToBooleans() {
-
-// don't change code above here
-
-  return true;
-
-// Only change code above this line.
-}
-
-welcomeToBooleans();
-
-var ourName = "Free Code Camp";
-var myName = "Dulio Denis";
+// name = "Alan Turing";
+// var firstName = "Alan";
+// var lastName = "Turing";
+var myFirstName = "Dulio";
+var myLastName = "Denis";
 
 // Only change code above this line.
 // We use this function to show you the value of your variable in your output box.
 // You'll learn about functions soon.
+if(typeof(myFirstName) !== "undefined" && typeof(myLastName) !== "undefined"){(function(){return myFirstName + ', ' + myLastName;})();}
 
-if(typeof(myName) !== "undefined"){(function(v){return v;})(myName);}
