3e40a280589542db6274a7a3f45d615b2dc94fab
Dulio Denis <dulio.denis@yahoo.com>
2015-09-07 14:36:11 -0400

    127. Add Two Numbers with JavaScript

--- 04-JavaScript/apps1.js
+++ 04-JavaScript/apps1.js
@@ -1,18 +1,7 @@
-var firstName = "Ada";
-
-var secondLetterOfFirstName = firstName[1];
-
-var lastName = "Lovelace";
-
-var thirdLetterOfLastName = lastName[2];
-
-// Use Bracket Notation to Find the Last Character in a String
-var lastLetterOfLastName = lastName[lastName.length - 1];
-
-// Use Bracket Notation to Find the NthtoLast Character in a String
-var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+var sum = 10 + 10; //make this equal to 20 by changing the 0 into the appropriate number.
+console.log(sum);
 
 // Only change code above this line.
 // We use this function to show you the value of your variable in your output box.
 // You'll learn about functions soon.
-(function(v){return v;})(thirdLetterOfLastName);
+(function(z){return 'sum='+z;})(sum);
