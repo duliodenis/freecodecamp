977244b0d28546c2e8e6242aecb3d83635e0e30e
Dulio Denis <dulio.denis@yahoo.com>
2015-09-07 13:51:42 -0400

    120. Declare JavaScript Variables

--- 04-JavaScript/apps1.js
+++ 04-JavaScript/apps1.js
@@ -8,3 +8,12 @@ function welcomeToBooleans() {
 }
 
 welcomeToBooleans();
+
+var ourName = "Free Code Camp";
+var myName = "Dulio Denis";
+
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+
+if(typeof(myName) !== "undefined"){(function(v){return v;})(myName);}
