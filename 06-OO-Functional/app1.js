c79098965bbf21fc32284f1ba6960b61ac94e65e
Dulio Denis <dulio.denis@yahoo.com>
2015-09-13 13:23:06 -0400

    174. Declare JavaScript Objects as Variables

---
+++ 06-OO-Functional/app1.js
@@ -0,0 +1,27 @@
+//
+//  Free Code Camp
+//  6. Object Oriented and Functional Programming
+//
+//  Created by Dulio Denis on 9/13/15.
+//  Copyright (c) 2015 ddApps. All rights reserved.
+//  ------------------------------------------------
+//  Waypoints: 12
+
+
+//  Waypoint 1: Declare JavaScript Objects as Variables
+//  http://freecodecamp.com/challenges/waypoint-declare-javascript-objects-as-variables
+//
+// Here is a sample Object
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+// Now Let's make a similar Object called motorBike
+// Give it two wheels, one engine and one seat
+var motorBike = {
+    wheels: 2,
+    engines: 1,
+    seats: 1
+};
